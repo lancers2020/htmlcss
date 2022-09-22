4c25d547c3f0bddce4aad9d81b3fd88612b9403b
davelimutin <dave.limutin.pixel8@gmail.com>
2022-09-22 16:48:20 +0800

    exercise 6 navbar function done

--- HOE_6_html_css/main.js
+++ HOE_6_html_css/main.js
@@ -2,3 +2,7 @@ const burger = document.querySelector('.burger');
 burger.addEventListener('click', function(){
     this.classList.toggle('is-active');
 })
+function mobileNav(){
+    const nav = document.getElementById('mobile');
+    nav.classList.toggle('is-mobile');
+}
