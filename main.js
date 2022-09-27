75039fc89fd9c7df92a3cd170583c0d4c64781e2
davelimutin <dave.limutin.pixel8@gmail.com>
2022-09-27 14:11:40 +0800

    navbar css modification

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ quit.addEventListener('click', function(){
     mobile.classList.remove('is-mobile');
     burger.classList.remove('is-active');
 })
+function changeNav(){
+    var navbar = document.getElementById('navUwu');
+    var scrollValue = window.scrollY;
+    if(scrollValue < 400){
+        navbar.classList.remove('navChange');
+    }
+    else{
+        navbar.classList.add('navChange');
+    }
+}
+window.addEventListener('scroll', changeNav);
